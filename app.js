562ed72a9a529e04f2e0822f8f70c1b33c62d417
LechCieslak <cieslak.lech@gmail.com>
2020-08-18 12:42:46 +0200

    for example

---
+++ app.js
@@ -0,0 +1,11 @@
+let a = document.querySelector('li');
+let b = document.querySelector('.oMnie');
+//let C = document.querySelector('.metoda');
+
+a.addEventListener('click', e => {
+  b.classList.toggle("nowa");
+});
+
+// a.addEventListener('click', e => {
+//   c.classList.toggle("nowa");
+// });
